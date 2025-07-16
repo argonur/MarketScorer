09ed1add895ee3accdba022a3f6fe3f5e750e1a2
Osvaldo Peralta <osvaldo.peralta@dye-industriales.com>
2025-07-16 11:40:19 -0600

    feat(indicators): Implementada la interfaz/clase abstracta IndicatorModule

---
+++ indicators/IndicatorModule.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractclassmethod
+class IndicatorModule(ABC):
+    """
+    Clase abstracta para todos los indicadores.
+    Es la interfaz que deben implementar.
+    """
+
+    @abstractclassmethod
+    def fetch_data(self):
+        """ Obtiene los datos necesarios para el indicador. """
+        pass
+
+    @abstractclassmethod
+    def normalize(self):
+        """ 
+        - Procesa los datos obtenidos y los transforma en valores entre 0 y 1.
+        - Este valor representa la contribución del indicador al sistema.
+        """
+    pass
+
+    def get_score(self):
+        """ 
+        - Retorna el valor normalizado.
+        - Este metodo puede ser sobreescrito si el indicador necesita ajustar el resultado.
+        """
+        return self.normalize()
