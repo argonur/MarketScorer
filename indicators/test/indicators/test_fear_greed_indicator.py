a0623b8d44b8b2fe088c239103e56433a85b0322
Osvaldo Peralta <osvaldo.peralta@dye-industriales.com>
2025-07-22 16:25:45 -0600

    Test para FearGreedIndicator organizados por nivel de complejidad

---
+++ indicators/test/indicators/test_fear_greed_indicator.py
@@ -0,0 +1,64 @@
+from unittest.mock import MagicMock
+from indicators.FearGreedIndicator import FearGreedIndicator
+import pytest
+
+from indicators.test.conftest import fetch_error
+
+######################### Test basicos para FearGreedIndicator #########################
+
+def test_fetch_data_valida(fetch_ok):
+    indicador = FearGreedIndicator(fetch_fn=fetch_ok)
+    resultado = indicador.fetch_data()
+    assert resultado.value == 70
+
+def test_normalize_valido(fetch_ok):
+    indicador = FearGreedIndicator(fetch_fn=fetch_ok)
+    assert indicador.normalize() == 0.7
+
+######################### Test intermedios para FearGreedIndicator #########################
+
+def test_fetch_data_error(fetch_error):
+    indicador = FearGreedIndicator(fetch_fn=fetch_error)
+    assert indicador.fetch_data() is None
+
+def test_normalize_falla(fetch_error):
+    indicador = FearGreedIndicator(fetch_fn=fetch_error)
+    assert indicador.normalize() == 0
+
+######################### Test avanzados para FearGreedIndicator #########################
+
+# Valores invalidos no esperados, fuera de rango
+@pytest.mark.parametrize("valor_fuera_de_rango", [-70, -1, -25, 105, 125])
+def test_valores_fuera_de_rango_lanza_excepcion(valor_fuera_de_rango):
+    class MockFGI:
+        def __init__(self, value):
+            self.value = value
+            self.description = "Test"
+            self.last_update = "2025-07-21"
+
+    fetch_mock_exception = lambda: MockFGI(valor_fuera_de_rango)
+    indicador = FearGreedIndicator(fetch_fn=fetch_mock_exception)
+
+    # verificamos que se lanza la excepcion al obtener datos fuera de rango
+    resultado = indicador.fetch_data()
+    assert resultado is None # Porque la excepcion se atrapa dentro de la clase y entonces devuelve un None en el Exception
+
+# Valores validos esperados, dentro del rango
+@pytest.mark.parametrize("valor, esperado", [
+    (75, 0.75),
+    (55, 0.55),
+    (25, 0.25),
+    (20, 0.2),
+    (100, 1.0),
+    (0, 0.0)
+])
+def test_normalize_parametrizado(valor, esperado):
+    mock_fgi = MagicMock
+    mock_fgi.value = valor
+    mock_fgi.description = "mock"
+    mock_fgi.last_update = "2025-07-21"
+
+    indicador = FearGreedIndicator(fetch_fn=lambda: mock_fgi)
+    assert indicador.normalize() == esperado
+
+# Se pueden agregar test parametrizados para normalize fuera de rango como en fetch_data
