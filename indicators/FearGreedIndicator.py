dda942613a352aebd92dcd5999092e8566bc94e0
Osvaldo Peralta <osvaldo.peralta@dye-industriales.com>
2025-07-29 14:55:35 -0600

    Reactorizacion de metodos, valor obtenido por la API redondeado al 0 mas cercano

--- indicators/FearGreedIndicator.py
+++ indicators/FearGreedIndicator.py
@@ -13,11 +13,12 @@ class FearGreedIndicator(IndicatorModule):
     def fetch_data(self): 
         try: 
             fgi = self.fetch_fn()
+            value = fgi.value.__round__() # Se redondea el valor de la API
 
             if not (0 <= fgi.value <= 100):
                 raise ValueError(f"Valor fuera de rango esperado: {fgi.value}")
 
-            print(f"Valor actual del CNN Fear & Greed Index: {fgi.value.__round__()} ({fgi.description})") 
+            print(f"Valor actual del CNN Fear & Greed Index: {value} ({fgi.description})")
             print(f"Última actualización: {fgi.last_update}") 
             return fgi
         except Exception as e: 
@@ -27,7 +28,7 @@ class FearGreedIndicator(IndicatorModule):
     def normalize(self):  
         data = self.fetch_data()
         if data and data.value is not None:
-            return data.value / 100 # Para obtener un numero entre 0 y 1 en decimal
+            return data.value.__round__() / 100 # Para obtener un numero entre 0 y 1 en decimal
         return 0
 
 if __name__ == "__main__":
