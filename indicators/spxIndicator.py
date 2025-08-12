7bc19da7b9de9e1b3d0f56489e9313340c40cdd5
Osvaldo Peralta <osvaldo.peralta@dye-industriales.com>
2025-08-12 14:22:58 -0600

    Implementado el indicador para calcular el SMA de SP-500

--- indicators/spxIndicator.py
+++ indicators/spxIndicator.py
@@ -6,31 +6,27 @@ import yfinance as yf
 config = get_config()
 # Obtenemos el valor del periodo para SMA desde el modulo de configuracion
 periodo_sma = config.get('indicators',{}).get('spx',{}).get('sma_period')
-symbol = "^SPX"
 
 class SPXIndicator(IndicatorModule):
     # Constructor
-    def __init__(self, config, ticker_factory=yf.Ticker):
-        """ Dependencias Inyectadas al indicador SPX.
-        parametro "config": Configuración del indicador
-        parametro "ticker_factory": Funcion para crear objetos Ticker
-        """
-        super().__init__()
-        self.config = config
-        self.ticker_factory = ticker_factory
+    def __init__(self, upper_ratio = None, lower_ratio = None, sma_period = None, yf_client = None, config_data = None):
+        """ El objetivo es poder crear un objeto yf_client falso para mockear en las pruebas. """
+        # Cargar configuracion mockeable para tests
+        self.config = config_data or get_config()
+        spx_config = self.config.get('indicators', {}).get('spx', {})
+        
+        self.upper_ratio = upper_ratio if upper_ratio is not None else spx_config.get('upper_ratio')
+        self.lower_ratio = lower_ratio if lower_ratio is not None else spx_config.get('lower_ratio')
+        self.sma_period = sma_period if sma_period is not None else spx_config.get('sma_period')
 
-        # Extracción de parametros de configuración
-        spx_config = config.get('indicators', {}).get('spx',{})
-        self.sma_period = spx_config.get('sma_period')
-        self.upper_ratio = spx_config.get('upper_ratio')
-        self.lower_ratio = spx_config.get('lower_ratio')
-        pass
+        # Cliente yf mockeable
+        self.yf_client = yf_client or yf
     
 ### Metodo independiente para obtener el ultimo cierre ###
-    def obtener_ultimo_cierre(self, symbol = symbol):
+    def obtener_ultimo_cierre(self):
         # Metodo para obtener el valor del ultimo cierre del indice S&P 500
         try:
-            sp500 = yf.Ticker(symbol)
+            sp500 = self.yf_client.Ticker("^SPX")
             hist = sp500.history(period="1d")
             if not hist.empty:
                 ultimo_cierre = hist['Close'].iloc[-1]
@@ -40,10 +36,11 @@ class SPXIndicator(IndicatorModule):
                 return None
         except Exception as e:
             print(f"Error: {e}")
+            raise
     
-    def fetch_data(self, symbol = symbol):
+    def fetch_data(self):
         try:
-            ticker = yf.Ticker(symbol)
+            ticker = self.yf_client.Ticker("^SPX")
 
             # Descargar 300 dias bursatiles para asegurar los dias por defecto
             historical_data = ticker.history(period="300d")
@@ -51,21 +48,24 @@ class SPXIndicator(IndicatorModule):
             if historical_data.empty:
                 raise ValueError("No se obtuvieron datos historicos")
             
+            # Verificar si la columna 'Close' existe
+            if 'Close' not in historical_data.columns:
+                raise ValueError("No se obtuvieron datos historicos")
+            
             # Obtener precios de cierres
             cierres = historical_data['Close']
-            if cierres.empty:
-                raise ValueError("No se obtuvieron datos historicos")
 
             # Verificar datos suficientes
-            if len(cierres) < periodo_sma:
+            if len(cierres) < self.sma_period:
                 available = len(cierres)
-                raise ValueError(f"Advertencia: Solo {available}/{periodo_sma} dias disponibles")
-            else:
-                sma = cierres.tail(periodo_sma).mean()
+                raise ValueError(f"Advertencia: Solo {available}/{self.sma_period} dias disponibles")
+                
+            sma = cierres.tail(self.sma_period).mean()
             return sma
 
         except Exception as e:
             print(f"Hubo un error al obtener datos de la API: {e}")
+            raise
     
     def normalize(self):
         try:
@@ -77,29 +77,23 @@ class SPXIndicator(IndicatorModule):
 
             if ultimo_cierre is None:
                 raise ValueError("No se pudo obtener el ultimo cierre")
-            upper_ratio = config.get('indicators',{}).get('spx',{}).get('upper_ratio')
-            lower_ratio = config.get('indicators',{}).get('spx',{}).get('lower_ratio')
-            score = None
 
             # Obtenemos el ratio
             ratio = (ultimo_cierre - sma) / sma
             # Evaluar y normalizar segun la formula
-            if ratio <= lower_ratio:
-                score = 1.0
-                return score
-            elif ratio >= upper_ratio:
-                score = 0.0
-                return score
-            elif lower_ratio < ratio < upper_ratio:
-                score = (upper_ratio - ratio) / (upper_ratio - lower_ratio)
-                return score
+            if ratio <= self.lower_ratio:
+                return 1.0
+            elif ratio >= self.upper_ratio:
+                return 0.0
+            elif self.lower_ratio < ratio < self.upper_ratio:
+                return (self.upper_ratio - ratio) / (self.upper_ratio - self.lower_ratio)
         except Exception as e:
             print(f"Hubo un error al normalizar los valores: {e}")
-            raise ValueError
+            raise
     
 if __name__ == "__main__":
     try:
-        indicador = SPXIndicator(config=config)
+        indicador = SPXIndicator()
         
         print(f"Periodo de SMA: {periodo_sma}")
         print(f"Calculo SMA-{periodo_sma} de SPX: {indicador.fetch_data():.2f}")
