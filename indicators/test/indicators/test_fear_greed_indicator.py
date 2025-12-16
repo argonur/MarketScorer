4d0a8462dd3629f0c0e15709108a049972e7e2c8
Osvaldo Peralta <osvaldo.peralta@dye-industriales.com>
2025-12-16 16:31:45 -0600

    FearGreed ahora soporta recibir una fecha como parametro. Se elimino un metodo no funcional. Se actualizaron sus respectivos tests

--- indicators/test/indicators/test_fear_greed_indicator.py
+++ indicators/test/indicators/test_fear_greed_indicator.py
@@ -1,29 +1,37 @@
+from email import message
 from unittest.mock import MagicMock
 from indicators.FearGreedIndicator import FearGreedIndicator
 import pytest
+from datetime import date
 
 from indicators.test.conftest import fetch_error
 
 ######################### Test basicos para FearGreedIndicator #########################
 
 def test_fetch_data_valida(fetch_ok):
+    fecha = date(2025, 12, 15) # Simulamos haber definido una fecha la cual recibira el indicador FearGreed cuando sea llamada en ScoreCalculator.
     indicador = FearGreedIndicator(fetch_fn=fetch_ok)
-    resultado = indicador.fetch_data()
+    resultado = indicador.fetch_data(fecha)
     assert resultado.value == 70
+    assert fecha == date(2025, 12, 15)
 
 def test_normalize_valido(fetch_ok):
+    fecha = date(2025, 12, 15)
     indicador = FearGreedIndicator(fetch_fn=fetch_ok)
-    assert indicador.normalize() == 0.3
+    assert fecha == date(2025, 12, 15)
+    assert indicador.normalize(fecha) == 0.3
 
 ######################### Test intermedios para FearGreedIndicator #########################
 
 def test_fetch_data_error(fetch_error):
+    fecha = date(2025, 12, 15)
     indicador = FearGreedIndicator(fetch_fn=fetch_error)
-    assert indicador.fetch_data() is None
+    assert indicador.fetch_data(fecha) is None
 
 def test_normalize_falla(fetch_error):
+    fecha = date(2025, 12, 15)
     indicador = FearGreedIndicator(fetch_fn=fetch_error)
-    assert indicador.normalize() == 0
+    assert indicador.normalize(fecha) == 0
 
 ######################### Test avanzados para FearGreedIndicator #########################
 
@@ -37,10 +45,11 @@ def test_valores_fuera_de_rango_lanza_excepcion(valor_fuera_de_rango):
             self.last_update = "2025-07-21"
 
     fetch_mock_exception = lambda: MockFGI(valor_fuera_de_rango)
+    fecha = MockFGI(valor_fuera_de_rango).last_update
     indicador = FearGreedIndicator(fetch_fn=fetch_mock_exception)
 
     # verificamos que se lanza la excepcion al obtener datos fuera de rango
-    resultado = indicador.fetch_data()
+    resultado = indicador.fetch_data(fecha)
     assert resultado is None # Porque la excepcion se atrapa dentro de la clase y entonces devuelve un None en el Exception
 
 # Valores invalidos manejados por normalize
@@ -52,9 +61,10 @@ def test_normalize_con_valores_invalidos(valor_invalido):
         last_update = "2025-07-22"
 
     fetch_mock = lambda: MockFGI(valor_invalido)
+    fecha = MockFGI(valor_invalido).last_update
     indicator = FearGreedIndicator(fetch_fn=fetch_mock)
 
-    score = indicator.normalize()
+    score = indicator.normalize(fecha)
     assert score == 0
 
 # Valores validos esperados, dentro del rango
@@ -73,7 +83,8 @@ def test_normalize_parametrizado(valor, esperado):
     mock_fgi.last_update = "2025-07-21"
 
     indicador = FearGreedIndicator(fetch_fn=lambda: mock_fgi)
-    assert indicador.normalize() == esperado
+    fecha = mock_fgi.last_update
+    assert indicador.normalize(fecha) == esperado
 
 # Test para comprobar el comportamiento del indicador despues del nuevo calculo
 def test_normalize_valor_medio():
@@ -83,22 +94,16 @@ def test_normalize_valor_medio():
     mock_fgi.last_update = "2025-07-21"
 
     indicador = FearGreedIndicator(fetch_fn=lambda: mock_fgi)
-    assert indicador.normalize() == 0.5  # (100 - 50) / 100
+    fecha = mock_fgi.last_update
+    assert indicador.normalize(fecha) == 0.5  # (100 - 50) / 100
 
 # Se pueden agregar test parametrizados para normalize fuera de rango como en fetch_data
 
-###### Test para el metodo get_current_indicator #####
-def test_get_current_indicator_valido(capsys):
-    mock_fgi = MagicMock()
-    mock_fgi.value = 53
-    mock_fgi.description = "mock"
-    mock_fgi.last_update = "2025-09-05"
-    
-    FearGreedIndicator(fetch_fn=lambda: mock_fgi).get_current_indicator()
-    captured = capsys.readouterr()
-    assert "Valor actual del CNN Fear & Greed" in captured.out # El primer print del metodo se valida
+########## Tests opcionales/temporales para los loggers ##########
 
-def test_get_current_indicator_none(capsys):
-    FearGreedIndicator(fetch_fn=lambda: None).get_current_indicator()
-    captured = capsys.readouterr()
-    assert "No se pudo obtener el valor de Fear & Greed" in captured.out # El mensaje de error se valida
+def test_logger_info_en_fetch_data(fetch_ok, caplog):
+    fecha = date(2025, 12, 15)
+    indicator = FearGreedIndicator(fetch_fn=fetch_ok)
+    with caplog.at_level("INFO"):
+        indicator.fetch_data(fecha)
+    assert any("Fecha a usar" in message for message in caplog.messages)
